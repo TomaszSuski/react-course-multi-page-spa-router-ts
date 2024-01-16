fcb3749feb1baf356d26bb065be88afc6e4ada92
Tomasz Suski <tomasz.suski@orba.co>
2024-01-16 15:00:47 +0100

    changed first child route to index route

--- src/routes.tsx
+++ src/routes.tsx
@@ -14,7 +14,7 @@ export const router = createBrowserRouter([
       // they are added direct;ly after domain name
       // absolute paths start with slash
       // they are added after domain name and before relative paths
-      { path: "/", element: <Home /> },
+      { index: true, element: <Home /> },
       { path: "/products", element: <Products /> },
       { path: "products/:productId", element: <ProductDetailsPage /> },
     ],
