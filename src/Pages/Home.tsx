3b69284ee8d7f3aefff5ecb5158e6c8adc549768
Tomasz Suski <tomasz.suski@orba.co>
2024-01-16 12:43:10 +0100

    added simple linking

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export interface HomeProps {}
 
 export default function Home(props: HomeProps) {
-  return <h1>Home page</h1>;
+  return (
+    <div>
+      <h1>Home page</h1>
+      <Link to="/products">Products page</Link>
+    </div>
+  );
 }
--- src/Pages/Products.tsx
+++ src/Pages/Products.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export interface ProductsProps {}
 
 export default function Products(props: ProductsProps) {
-  return <h1>Products</h1>;
+  return <div><h1>Products</h1>
+  <Link to="/">Home Page</Link></div>;
 }
