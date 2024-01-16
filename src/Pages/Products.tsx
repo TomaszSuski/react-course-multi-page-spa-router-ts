dde780dd973ad637ad25eb47822215bd71b20392
Tomasz Suski <tomasz.suski@orba.co>
2024-01-16 12:35:41 +0100

    added second route

---
+++ src/Pages/Products.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export interface ProductsProps {}
+
+export default function Products(props: ProductsProps) {
+  return <h1>Products</h1>;
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./Pages/Home";
+import Products from "./Pages/Products";
 
-export const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+export const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Products /> },
+]);
