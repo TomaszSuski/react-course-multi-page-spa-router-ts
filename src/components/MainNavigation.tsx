fbae218980d70c665d697f2ff53c7411b899d8c2
Tomasz Suski <tomasz.suski@orba.co>
2024-01-16 14:12:27 +0100

    chengeg Links in navbar to NavLinks and used it's extra features

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 
 export interface MainNavigationProps {}
@@ -10,10 +10,31 @@ export default function MainNavigation(props: MainNavigationProps) {
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              // NavLink przyjmuje specjalny rodzaj propa className, który jest funkcją
+              // która przyjmuje obiekt z informacją czy dany link jest aktywny czy nie
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              // prop end zapewnia, że link nie będzie aktywny, jeśli aktywują się jego dzieci (dalszy ciąg ścieżki)
+              // czyli jeśli aktywny będzie link /products/1 to link /products nie będzie aktywny
+              // dzięki temu nie będziemy mieli dwóch aktywnych linków na raz
+              // choć w tym przypadku działa to natywnie również bez tego propa
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
