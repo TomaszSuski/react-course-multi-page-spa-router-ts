8f04e0f3c8ed5d2cb0ca2b3f1fdcfa3a3e046a32
Tomasz Suski <tomasz.suski@orba.co>
2024-01-16 13:03:38 +0100

    added simple content styling

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router";
 import MainNavigation from "../components/MainNavigation";
+import classes from "./Root.module.css";
 
 export interface RootLayoutProps {}
 
@@ -8,7 +9,9 @@ export default function RootLayout(props: RootLayoutProps) {
   return (
     <>
       <MainNavigation />
-      <Outlet />
+      <main className={classes.content}>
+        <Outlet />
+      </main>
     </>
   );
 }
