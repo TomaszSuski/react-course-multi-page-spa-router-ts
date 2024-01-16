26691917fb4ef44ba2ed1d70c5672d364ec3206a
Tomasz Suski <tomasz.suski@orba.co>
2024-01-16 14:26:54 +0100

    added useNavigate usage example

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export interface HomeProps {}
 
 export default function Home(props: HomeProps) {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate("/products");
+  }
+
   return (
     <>
       <h1>Home page</h1>
       <Link to="/products">Products page</Link>
+      <button onClick={navigateHandler}>Navigate to products</button>
     </>
   );
 }
